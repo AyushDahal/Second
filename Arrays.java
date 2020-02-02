118b6a331667a448f539da1d7aaf956914074ef9
AyushDahal <ayushdahal533@gmail.com>
2020-02-02 17:10:48 +0545

    added

---
+++ Arrays.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myproj;
+
+/**
+ *
+ * @author ayushpc
+ */
+public class Arrays {
+    
+    public static void main(String[] args) {
+        
+        int a[]={1,2,3,4,5};
+        int b[]=new int [5];
+        
+        for(int i=0;i<b.length;i++)
+        {
+        b[i]=a[i]+1;
+        
+            System.out.println(b[i]);
+        
+        }
+        
+        
+        
+    }
+        
+        
+        
+        
+    }
+    
+    
+
