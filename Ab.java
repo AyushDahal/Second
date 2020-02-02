532f8a1b407a5b7b92dae93a953806d7b91337f2
AyushDahal <ayushdahal533@gmail.com>
2020-02-02 08:50:10 +0545

    sent

---
+++ Ab.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myproj;
+
+/**
+ *
+ * @author ayushpc
+ */
+public class Ab {
+    
+    
+    public static void main(String[] args) {
+        
+        int a[]={1,2,3,4,5};
+        
+        for(int i=0;i<a.length;i++)
+        {
+        
+            if(a[i]==3)
+            {
+            continue;
+            }
+            System.out.println(a[i]);
+        }
+        
+        
+        
+        
+    }
+    
+    
+    
+}
