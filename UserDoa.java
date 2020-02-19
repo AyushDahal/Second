4b6102751a290845b8f7ac8dad67da3c052cda5f
AyushDahal <ayushdahal533@gmail.com>
2020-02-19 16:29:48 +0545

    added

---
+++ UserDoa.java
@@ -0,0 +1,29 @@
+package com.ayu.database;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface UserDoa {
+
+    @Query("SELECT * FROM users")
+    List<Users> getAll();
+
+    @Query("SELECT*FROM users WHERE id IN(:userIds)")
+    List<Users> loadAllByIds(int[] userIds);
+
+    @Query("SELECT* FROM users WHERE name LIKE:name AND "+" address LIKE :address LIMIT 1")
+    Users findByName(String name, String address);
+
+    @Insert
+    void insertAll(Users... users);
+
+    @Delete
+    void delete(Users... users);
+
+
+}
